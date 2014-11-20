e1ffcbcfb04129b04bb2032b6067c41205317a0b
wonder1234 <xiangjia@0587337334.wireless.umich.net>
2014-11-20 15:59:10 -0500

    add a function
    int f() {
    }

---
+++ blabla/blabla/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  blabla
+//
+//  Created by Jane Xiang on 11/20/14.
+//  Copyright (c) 2014 Jane Xiang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    cout << "((((";
+    return 0;
+}
